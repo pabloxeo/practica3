b080eca4cbb2e467d678cc907be3ef4a15f86293
rbnuria <rbnuria6@gmail.com>
2022-04-29 12:24:37 +0200

    Ahora si

---
+++ include/Ninja.h
@@ -0,0 +1,14 @@
+#ifndef __NINJA__
+#define __NINJA__
+
+# include "Attributes.h"
+# include "AIPlayer.h"
+
+
+class Ninja: public AIPlayer{
+    public:
+        inline Ninja(const string & name):AIPlayer(name){};
+        void think(color & c_piece,  int & id_piece, int & dice) const;
+};
+
+#endif
